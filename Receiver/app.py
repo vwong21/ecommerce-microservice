0b621c6beb73abc66064d057701b5df4d6e2e9a3
Vincent Wong <vincentwong5609@gmail.com>
2024-04-03 21:35:05 -0700

    Start Kafka on Receiver start

--- Receiver/app.py
+++ Receiver/app.py
@@ -7,10 +7,12 @@ import requests
 import yaml
 import logging
 import logging.config
+import time
 import uuid
 
 app = connexion.FlaskApp(__name__, specification_dir="")
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+logging.info("Connected on port 8080")
 
 MAX_EVENTS = 5
 EVENT_FILE = "events.json"
@@ -24,10 +26,11 @@ logger = logging.getLogger("basicLogger")
 with open("app_conf.yaml", "r") as f:
     app_config = yaml.safe_load(f.read())
 
-
-def send_to_kafka(event_type, event_data):
+retry_count = 0
+max_retries = app_config["events"]["max_retries"]
+producer = None
+while retry_count < max_retries:
     try:
-
         events = app_config["events"]
         kafka_server = events["hostname"]
         kafka_port = events["port"]
@@ -36,7 +39,18 @@ def send_to_kafka(event_type, event_data):
         client = KafkaClient(hosts=f"{kafka_server}:{kafka_port}")
         topic = client.topics[str.encode(kafka_topic)]
         producer = topic.get_sync_producer()
+        logging.info(f"Successfully Connected to Kafka on attempt {retry_count}")
 
+        break
+    except Exception as e:
+        logging.info(f"Connection to Kafka failed on attempt {retry_count}")
+        retry_count += 1
+        sleep_time = app_config["events"]["retry_sleep_value"]
+        time.sleep(sleep_time)
+
+
+def send_to_kafka(event_type, event_data, producer):
+    try:
         trace_id = str(uuid.uuid4())
         event_data["trace_id"] = trace_id
 
@@ -54,12 +68,12 @@ def send_to_kafka(event_type, event_data):
 
 
 def createProduct(body):
-    send_to_kafka("products", body)
+    send_to_kafka("products", body, producer)
     return {"message": "Product creation request received successfully"}, 201
 
 
 def processOrder(body):
-    send_to_kafka("orders", body)
+    send_to_kafka("orders", body, producer)
     return {"message": "Order creation request received successfully"}, 201
 
 
