d119c6aab6dabc18454fba3a47705f54491f340f
Vincent Wong <vincentwong5609@gmail.com>
2024-04-07 17:44:54 -0700

    added correct endpoints to react app

--- Dashboard-UI/src/components/AppStats.js
+++ Dashboard-UI/src/components/AppStats.js
@@ -8,7 +8,7 @@ export default function AppStats() {
 
 	const getStats = () => {
 	
-        fetch(`http://kafka-acit3855-lab6.eastus.cloudapp.azure.com:8100/stats`)
+        fetch(`http://kafka-acit3855-lab6.eastus.cloudapp.azure.com/processing/stats`)
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Stats")
--- Dashboard-UI/src/components/EndpointAudit.js
+++ Dashboard-UI/src/components/EndpointAudit.js
@@ -9,7 +9,7 @@ export default function EndpointAudit(props) {
 	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
 
     const getAudit = () => {
-        fetch(`http://kafka-acit3855-lab6.eastus.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
+        fetch(`http://kafka-acit3855-lab6.eastus.cloudapp.azure.com/audit/${props.endpoint}?index=${rand_val}`)
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Audit Results for " + props.endpoint)
